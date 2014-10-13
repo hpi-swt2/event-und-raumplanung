9b22dcbc37f2ab0981882413d362c25eeb512031
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2014-10-13 19:59:22 +0200

    Route / to /events

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
