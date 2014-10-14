1b0a4c95f3093788b8da3905c66b31a20e0bd009
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2014-10-14 13:20:59 +0200

    Updated comment on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # If you add an option, be sure to inspect the migration file
-  # "add_devise_to_user" and uncomment the appropriate section
+  # "devise_create_user" and uncomment the appropriate section
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
