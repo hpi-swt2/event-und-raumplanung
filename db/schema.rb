a60676bf8cf2b2d89bc05e6e1d9a46d49dfcabdd
Meike Baumgärtner <meike.baumgaertner@googlemail.com>
2014-11-07 19:13:29 +0100

    Change devise user model and login, ref #9
      * add string identity_url to model
      * remove undesired devise options (like registration)
      * change sing_in view to openid only

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20141014105900) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "identity_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
