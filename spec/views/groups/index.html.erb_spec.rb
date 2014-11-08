128b3dda72dde4a17f788f70d4bdb311836840f5
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2014-11-08 18:04:51 +0100

    Modify groups view spec ref #20

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe "groups/index", :type => :view do
         :name => "Name"
       ),
       Group.create!(
-        :name => "Name"
+        :name => "Test"
       )
     ])
   end
 
   it "renders a list of groups" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => "Test".to_s, :count => 1
   end
 end
