9863f06bdd9e360f7e15dcfe85615da2ea7b2671
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2014-11-11 00:21:08 +0100

    #22 Updated/Wrote Tests. Everything passed. Travis should stop bitching around ;) Issue should be completed with that.

--- test/controllers/room_properties_controller_test.rb
+++ test/controllers/room_properties_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class RoomPropertiesControllerTest < ActionController::TestCase
+  setup do
+    @roomProperty = room_properties(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:roomProperties)
   end
 
   test "should get new" do
@@ -11,14 +16,34 @@ class RoomPropertiesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
+  test "should create roomProperty" do
+    assert_difference('RoomProperty.count') do
+      post :create, room_property: { name: @roomProperty.name }
+    end
+
+    assert_redirected_to room_properties_path
+  end
+
+  test "should show roomProperty" do
+    get :show, id: @roomProperty
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @roomProperty
     assert_response :success
   end
 
+  test "should update roomProperty" do
+    patch :update, id: @roomProperty, room_property: { name: @roomProperty.name }
+    assert_redirected_to room_properties_path
+  end
+
+  test "should destroy roomProperty" do
+    assert_difference('RoomProperty.count', -1) do
+      delete :destroy, id: @roomProperty
+    end
+
+    assert_redirected_to room_properties_path
+  end
 end
