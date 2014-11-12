6926978c89d3a773992b1364a1aaf2867af45193
Philipp Schirmer <philipp@schirmer-online.de>
2014-11-13 00:04:36 +0100

    #12 Added method to return upcoming events uniformly

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ActiveRecord::Base
   has_many :bookings
   has_many :equipment # The plural of 'equipment' is 'equipment'
+
+  def upcoming_events
+    return self.bookings.where(['start > ?', DateTime.now]).order('start asc')
+  end
+
 end
