eab2aecd792bf06e45f83bb7ae3376ed243c167f
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2014-11-13 12:36:59 +0100

    Added the is_private field for the events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107161930) do
+ActiveRecord::Schema.define(version: 20141113112042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20141107161930) do
     t.time     "end_time"
     t.integer  "user_id"
     t.integer  "room_id"
+    t.boolean  "is_private"
   end
 
   add_index "events", ["room_id"], name: "index_events_on_room_id", using: :btree
