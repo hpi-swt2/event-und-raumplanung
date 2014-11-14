00ee40f68cdd6ff0014526b8526271605db038cf
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-11-14 17:36:55 +0100

    try tests 2

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe TasksController do
+  describe "new" do
+    before  { set_current_user FactoryGirl.create(:user) }
+
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+
+  factory :user do
+    email         'user@example.org'
+  end
+end
