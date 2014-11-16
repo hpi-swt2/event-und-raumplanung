470635887d0a65a2e2f98181176c086450c1982a
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-11-16 17:24:36 +0100

    Added approved status to events model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112125005) do
+ActiveRecord::Schema.define(version: 20141116153048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.time     "end_time"
     t.integer  "user_id"
     t.integer  "room_id"
+    t.boolean  "approved"
   end
 
   add_index "events", ["room_id"], name: "index_events_on_room_id", using: :btree
