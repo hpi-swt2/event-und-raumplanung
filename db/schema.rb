d5b48ba4e23707abb0acf0730c410bd28fbd1add
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-11-18 16:56:16 +0100

    schema.rb for merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107153803) do
+ActiveRecord::Schema.define(version: 20141114091730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "bookings", force: true do |t|
     t.string   "name"
@@ -63,9 +62,12 @@ ActiveRecord::Schema.define(version: 20141107153803) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "done",        default: false
+    t.integer  "user_id"
+    t.string   "status"
   end
 
   add_index "tasks", ["event_id"], name: "index_tasks_on_event_id", using: :btree
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
