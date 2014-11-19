af1daff0ec2ddee80c79502fb2f2917b769c97b6
Janos Brauer <janos.brauer@student.hpi.uni-potsdam.de>
2014-11-19 18:28:48 +0100

    added status to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107224844) do
+ActiveRecord::Schema.define(version: 20141119172205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20141107224844) do
     t.time     "end_time"
     t.integer  "user_id"
     t.integer  "room_id"
+    t.string   "status",            default: "In Bearbeitung"
   end
 
   add_index "events", ["room_id"], name: "index_events_on_room_id", using: :btree
