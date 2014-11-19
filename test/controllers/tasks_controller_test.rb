d34ce069fd22c4bf80efbcc2ddf15647a2763f31
Sebastian Kliem <sebastian-kliem@live.de>
2014-11-19 15:26:34 +0100

    extend TaskController tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -18,7 +18,7 @@ class TasksControllerTest < ActionController::TestCase
 
   test "should create task" do
     assert_difference('Task.count') do
-      post :create, task: { description: @task.description, event_id: @task.event_id, name: @task.name }
+      post :create, task: { description: @task.description, event_id: @task.event_id, name: @task.name, done: @task.done }
     end
 
     assert_redirected_to task_path(assigns(:task))
@@ -35,7 +35,7 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should update task" do
-    patch :update, id: @task, task: { description: @task.description, event_id: @task.event_id, name: @task.name }
+    patch :update, id: @task, task: { description: @task.description, event_id: @task.event_id, name: @task.name, done: @task.done }
     assert_redirected_to task_path(assigns(:task))
   end
 
