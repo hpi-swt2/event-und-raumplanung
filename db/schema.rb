e933de843dbe3c8fd448da2c529c64ea000c89dc
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2014-11-20 13:47:28 +0100

    implemented the room list with filter

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112125005) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-  enable_extension "hstore"
+ActiveRecord::Schema.define(version: 20141120111043) do
 
   create_table "bookings", force: true do |t|
     t.string   "name"
@@ -28,8 +24,8 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.datetime "updated_at"
   end
 
-  add_index "bookings", ["event_id"], name: "index_bookings_on_event_id", using: :btree
-  add_index "bookings", ["room_id"], name: "index_bookings_on_room_id", using: :btree
+  add_index "bookings", ["event_id"], name: "index_bookings_on_event_id"
+  add_index "bookings", ["room_id"], name: "index_bookings_on_room_id"
 
   create_table "equipment", force: true do |t|
     t.string   "name"
@@ -40,7 +36,7 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.string   "category"
   end
 
-  add_index "equipment", ["room_id"], name: "index_equipment_on_room_id", using: :btree
+  add_index "equipment", ["room_id"], name: "index_equipment_on_room_id"
 
   create_table "event_templates", force: true do |t|
     t.string   "name"
@@ -55,8 +51,8 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.integer  "room_id"
   end
 
-  add_index "event_templates", ["room_id"], name: "index_event_templates_on_room_id", using: :btree
-  add_index "event_templates", ["user_id"], name: "index_event_templates_on_user_id", using: :btree
+  add_index "event_templates", ["room_id"], name: "index_event_templates_on_room_id"
+  add_index "event_templates", ["user_id"], name: "index_event_templates_on_user_id"
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -72,8 +68,15 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.integer  "room_id"
   end
 
-  add_index "events", ["room_id"], name: "index_events_on_room_id", using: :btree
-  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
+  add_index "events", ["room_id"], name: "index_events_on_room_id"
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "room_properties", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "room_id"
+  end
 
   create_table "rooms", force: true do |t|
     t.string   "name"
@@ -90,7 +93,7 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.datetime "updated_at"
   end
 
-  add_index "tasks", ["event_id"], name: "index_tasks_on_event_id", using: :btree
+  add_index "tasks", ["event_id"], name: "index_tasks_on_event_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -107,7 +110,7 @@ ActiveRecord::Schema.define(version: 20141112125005) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
