97634248915924716811dffa2ca2b734bd96b6cc
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-11-20 04:33:10 +0100

    added dependencies between event and user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   has_many :bookings
   has_many :tasks
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # If you add an option, be sure to inspect the migration file
   # "devise_create_user" and uncomment the appropriate section
+  has_many :events
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
