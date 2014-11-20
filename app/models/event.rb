70458cca69a100c23b13924a1e2ba48887d3dda7
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-11-20 18:44:48 +0100

    Revert "added dependencies between event and user"
    This reverts commit 97634248915924716811dffa2ca2b734bd96b6cc.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
   has_many :bookings
   has_many :tasks
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20141116153048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "bookings", force: true do |t|
     t.string   "name"
