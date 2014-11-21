08a03011d26852521679dd30b8580b5c73c3259e
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-11-21 02:28:33 +0100

    updated nil checks of events and bookings

--- app/controllers/approveevents_controller.rb
+++ app/controllers/approveevents_controller.rb
@@ -13,24 +13,22 @@ class ApproveeventsController < ApplicationController
   		@bookings = Booking.all
   		@events = Event.all
   		check_data # data must be checked before selection criterea as events links to bookings which are filtered out
-  		@events = @events.where(approved: nil)
+  		@events.where!(approved: nil)
   		@bookings.where!('start BETWEEN ? AND ?', @date.beginning_of_day, @date.end_of_day).order(:start, :event_id)
   end
 
-  def check_data
-  	@events.where(name: nil).select(:id).each do |event_id|
-  		@events.find(event_id).name = 'generated name'  #unless event.update_attribute.nil?
-  	end
-  	@events.where(description: nil).update_all(:description => 'generated description')
-  	@events.where(start_time: nil).update_all(:start_time => Time.current)
-  	@events.where(end_time: nil).update_all(:end_time => Time.current)
-   	@events.where(start_date: nil).update_all(:start_date => Date.today)
-  	@events.where(end_date: nil).update_all(:end_date => Date.today)
+  def check_data # the validation of the data should move to the controller in the future
+  	@events.where(name: nil).update_all(:name => '')
+  	@events.where(description: nil).update_all(:description => '')
+  	@events.where(start_time: nil).update_all(:start_time => Time.new)
+  	@events.where(end_time: nil).update_all(:end_time => Time.new)
+   	@events.where(start_date: nil).update_all(:start_date => Date.new)
+  	@events.where(end_date: nil).update_all(:end_date => Date.new)
 
-	@bookings.where(start: nil).update_all(:start => DateTime.current)
-	@bookings.where(end: nil).update_all(:end => DateTime.current)
-	@bookings.where(name: nil).update_all(:name => 'generated name')
-	@bookings.where(description: nil).update_all(:description => 'generated description')
+  	@bookings.where(start: nil).update_all(:start => DateTime.new)
+	@bookings.where(end: nil).update_all(:end => DateTime.new)
+	@bookings.where(name: nil).update_all(:name => '')
+	@bookings.where(description: nil).update_all(:description => '')
 	@bookings.where(room_id: nil).update_all(:room_id => Room.first.id)
 	@bookings.where(event_id: nil).update_all(:event_id => Event.first.id)
   end
