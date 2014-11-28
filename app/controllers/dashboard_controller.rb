a50c3cdf1acb3c756cc822f77b0bac469549bc78
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-11-28 13:43:10 +0100

    fix shown events

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,6 @@ class DashboardController < ApplicationController
   private
 
     def next_five_events
-      return Event.where("starts_at <= '#{(Time.current.to_s(:db))}'").order('starts_at ASC').limit(5)
+      return Event.where("starts_at >= '#{(Time.current.to_s(:db))}'").order('starts_at ASC').limit(5)
     end
 end
