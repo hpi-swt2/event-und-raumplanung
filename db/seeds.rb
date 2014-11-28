2de32da01288812e46fa8664173b209c78c8827f
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-11-28 10:52:47 +0100

    add seeds for tasks, fix show task second attempt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create({:name => 'A Task', :description => 'This is a task.', :status => "not_assigned"})
