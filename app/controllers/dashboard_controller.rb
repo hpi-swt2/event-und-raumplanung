4d0839d386eac0d82b7318dc3665dbed12e3604e
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-11-28 13:38:39 +0100

    add overview for upcoming events in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class DashboardController < ApplicationController
 
 	def index
+    @events = next_five_events
 		
-	end
+  end
+
+  private
+
+    def next_five_events
+      return Event.where("starts_at >= '#{(Time.current.to_s(:db))}'").order('starts_at ASC').limit(5)
+    end
 end
