0b710b90f3fd8cefbd980418830d4a9b0eaa1651
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-02 10:35:50 +0100

    Adapted changed requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :setup_filterrific
 
   protected
   def authenticate_user!
@@ -16,24 +15,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     stored_location_for(:user)
-  end
-
-  def setup_filterrific
-    @filterrific = Filterrific.new(
-    Event,
-    params[:filterrific] || session[:filterrific_events])
-    @filterrific.select_options =   {
-      sorted_by: Event.options_for_sorted_by
-    }
-    @filterrific.own = if @filterrific.own == 1
-        current_user_id
-      else
-        nil
-      end
-    @filterrific.room_ids = Room.all.map(&:id) if @filterrific.room_ids && @filterrific.room_ids.size <=1
-
-    session[:filterrific_events] = @filterrific.to_hash
-      
   end
 
   # Link to root if a access denied exception by cancancan occurs
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,7 +24,21 @@ class EventsController < ApplicationController
   def index
 
      
-     @events = Event.filterrific_find(@filterrific).page(params[:page])
+     @filterrific = Filterrific.new(
+      Event,
+      params[:filterrific] || session[:filterrific_events])
+      @filterrific.select_options =   {
+        sorted_by: Event.options_for_sorted_by
+      }
+      @filterrific.own = if @filterrific.own == 1
+          current_user_id
+        else
+          nil
+        end
+      @filterrific.room_ids = Room.all.map(&:id) if @filterrific.room_ids && @filterrific.room_ids.size <=1
+      @events = Event.filterrific_find(@filterrific).page(params[:page])
+
+      session[:filterrific_events] = @filterrific.to_hash
 
 
     respond_to do |format|
