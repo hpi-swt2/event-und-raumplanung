8082570d758763313a5c601edcc3f547088cbe6c
Dominic Sauer <dominic.sauer@student.hpi.de>
2014-12-05 16:23:44 +0100

    #54 use icon for file uploads too

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,10 @@
 module TasksHelper
+
   def public_url(file_id, file_name)
     return "/files/" + file_id.to_s + "/" + file_name
+  end
+
+  def has_attached_items?(task)
+    return task.attachments.count() > 0 || task.uploads.count() > 0
   end
 end
