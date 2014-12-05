588febf76f000bac00c0530ce8004932ec127fc7
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-12-05 17:08:49 +0100

    removed validation of data from events approval controller

--- app/controllers/events_approval_controller.rb
+++ app/controllers/events_approval_controller.rb
@@ -15,26 +15,8 @@ class EventsApprovalController < ApplicationController
 
 
   def index
-		@bookings = Booking.all
-		@events = Event.all
-		check_data # data must be checked before selection criterea as events links to bookings which are filtered out
-		@events = @events.where.not({status: ['approved', 'declined']})
-		@bookings.where!('start BETWEEN ? AND ?', @date.beginning_of_day, @date.end_of_day).order(:start, :event_id)
+		@bookings = Booking.where('start BETWEEN ? AND ?', @date.beginning_of_day, @date.end_of_day).order(:start, :event_id)
+		@events = Event.where.not({status: ['approved', 'declined']})
   end
 
-  def check_data # the validation of the data should move to the model in the future
-	@events.where(name: nil).update_all(:name => '')
-	@events.where(description: nil).update_all(:description => '')
-	@events.where(start_time: nil).update_all(:start_time => Time.new)
-	@events.where(end_time: nil).update_all(:end_time => Time.new)
-	@events.where(start_date: nil).update_all(:start_date => Date.new)
-	@events.where(end_date: nil).update_all(:end_date => Date.new)
-
-	@bookings.where(start: nil).update_all(:start => DateTime.new)
-	@bookings.where(end: nil).update_all(:end => DateTime.new)
-	@bookings.where(name: nil).update_all(:name => '')
-	@bookings.where(description: nil).update_all(:description => '')
-	@bookings.where(room_id: nil).update_all(:room_id => Room.first.id)
-	@bookings.where(event_id: nil).update_all(:event_id => Event.first.id)
-  end
 end
