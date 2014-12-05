80b54e5f6d6504524da83cd3f0d6b3278278fc75
Janos Brauer <janos.brauer@student.hpi.uni-potsdam.de>
2014-12-05 13:42:14 +0100

    some javascript stuff for checking event conflicts

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,4 +2,16 @@
 // // All this logic will automatically be available in application.js.
 
 
+$(document).ready(function() { 
+	$('#sugguest-form input').change(function() { 
+		$.ajax({
+			url: taskPath,
+			type: 'PUT',
+			data: {task: {done: target.checked}},
+			dataType: 'json'
+		})
+}); 
+
+}); 
+
 
