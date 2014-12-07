4a209513103f07754c341afe6bd0bd4307055933
SvenLehmann <sven@Svens-MBP.fritz.box>
2014-12-07 16:34:36 +0100

    deadline can now be saved and UI improvement

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -109,10 +109,10 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :description, :event_id, :user_id, :done)
+      params.require(:task).permit(:name, :description, :event_id, :user_id, :done, :deadline)
     end
     def task_params_with_attachments
-      params.require(:task).permit(:name, :description, :event_id, :user_id, :done, :attachments_attributes => [ :title, :url ])
+      params.require(:task).permit(:name, :description, :event_id, :user_id, :done, :deadline, :attachments_attributes => [ :title, :url ])
     end
     def event_id
       if params[:event]
