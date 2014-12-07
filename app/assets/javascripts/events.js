324a184b8c1c235bee2f839e42eefe1241c2140d
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-07 14:06:12 +0100

    UI improvements for #104

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,5 +1,12 @@
 // // Place all the behaviors and hooks related to the matching controller here.
 // // All this logic will automatically be available in application.js.
 
-
-
+$(function() {
+  $('#advancedSearch').on('shown.bs.collapse', function () {
+    $(".drop-down-chevron").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+  });
+  
+  $('#advancedSearch').on('hidden.bs.collapse', function () {
+    $(".drop-down-chevron").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+  });
+});
