b1beb7012a4f5255ea58ea7acce6a3fd4152687b
Fabian Wiebe <fabian.wiebe@student.hpi.uni-potsdam.de>
2014-12-07 18:21:01 +0100

    added rights for admin users to approve and decline events

--- app/controllers/events_approval_controller.rb
+++ app/controllers/events_approval_controller.rb
@@ -1,22 +1,33 @@
 class EventsApprovalController < ApplicationController
 
-	before_action :read_and_exec_params
-    def read_and_exec_params
-        if params[:date]
-            if params[:date].is_a?(Hash)
-                @date = (params[:date][:year]+ '-' + params[:date][:month] + '-' + params[:date][:day]).to_date
-                else
-                @date = params[:date].to_date
-            end
-            else	
-            @date = Date.today
-        end
-    end
-
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  skip_load_and_authorize_resource :only =>[:index] #this line needs to be removed in the future as only admins should be able to view this page
 
   def index
+    read_and_exec_params
+    check_admin_status
 		@bookings = Booking.where('start BETWEEN ? AND ?', @date.beginning_of_day, @date.end_of_day).order(:start, :event_id)
 		@events = Event.where.not({status: ['approved', 'declined']})
   end
+
+  private
+    def read_and_exec_params
+      if params[:date]
+        if params[:date].is_a?(Hash)
+          @date = (params[:date][:year]+ '-' + params[:date][:month] + '-' + params[:date][:day]).to_date
+        else
+          @date = params[:date].to_date
+        end
+      else    
+        @date = Date.today
+      end
+    end
+
+    def check_admin_status
+      config = YAML.load_file(Rails.root.join('config', 'config.yml'))
+      admin_identity = config['admin']['identity_url']
+      @user_is_admin = (current_user.identity_url == admin_identity)
+    end
 
 end
