312a036d2fe445a0421b335c56bc184fdbf5f179
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2014-12-09 13:28:54 +0100

    added test for rooms list view

--- spec/factories/room_property.rb
+++ spec/factories/room_property.rb
@@ -2,4 +2,10 @@ FactoryGirl.define do
   factory :room_property do |f|
     f.name "Property"
   end
+  factory :room_property1, class: RoomProperty do
+                           name 'barrierfree'
+                         end
+                         factory :room_property2, class: RoomProperty do
+                                                  name 'barrierfree2'
+                                                end
 end
