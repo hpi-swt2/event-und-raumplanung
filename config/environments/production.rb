84239e934734a27cddd566f97c98d82ae5f0ff51
claudia.kanter <claudia.kanter@student.hpi.uni-potsdam.de>
2014-12-10 08:42:41 +0100

    changed smtp settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'event-und-raumplanung.herokuapp.com', port: 80 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp-mail.outlook.com',
-    port:                 587,
-    user_name:            'event-und-raumplanung@outlook.com',
-    password:             'Test1234',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = {from: 'event-und-raumplanung@outlook.com'}
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
