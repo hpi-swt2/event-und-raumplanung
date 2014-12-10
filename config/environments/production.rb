fa61876e042c5ab4358c521f1a60d6b20d6ef70d
Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>
2014-12-10 13:10:49 +0100

    Disable mail delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_options = {from: ENV['SENDGRID_USERNAME']}
 end
