5f45c91ff25eb29657c224ff349a02726d977863
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-16 11:03:02 +0100

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20141210144902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "attachments", force: true do |t|
     t.string   "title"
