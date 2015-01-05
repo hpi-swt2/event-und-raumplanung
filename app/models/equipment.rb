fbd5d9e5db4244c83c045b70d4907304c444a6f5
Sebastian.Rehfeldt <sebastian.rehfeldt@gmx.net>
2015-01-05 18:08:14 +0100

    improved filter for names; added validations for attributes when creating equipments

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,12 @@
 class Equipment < ActiveRecord::Base
   belongs_to :room
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :room_id, presence: true
+  validates :category, presence: true
+  
+
   filterrific(
     filter_names: [
       :equipment_name,
@@ -9,12 +15,14 @@ class Equipment < ActiveRecord::Base
     ]
   )
   scope :equipment_name, lambda { |name|
-    where('name = ?', name)
+    terms = name.downcase.split(/\s+/)
+    terms = terms.map { |e| (e.gsub('*', '%') + '%').gsub(/%+/, '%')}
+    where( terms.map { |term| "LOWER(equipment.name) LIKE ?"}.join(' AND '), *terms.map { |e| [e]} )
   }
   scope :rooms, lambda { |room|
   	
     room_id = Room.find_by_name(room)
-    where('room_id = ?', room_id)
+    where('room_id = ?', room_id) 
   }
 
   scope :category, lambda { |category|
