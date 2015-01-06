f730aadb3b7781312d4d35c0c7ad55bef9aa350e
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-01-06 16:07:24 +0100

    Update Activity migrations

--- db/migrate/20150106110346_create_activities.rb
+++ db/migrate/20150106110346_create_activities.rb
@@ -3,6 +3,7 @@ class CreateActivities < ActiveRecord::Migration
     create_table :activities do |t|
       t.string :username
       t.string :action
+      t.text :changed_fields
 
       t.timestamps
     end
