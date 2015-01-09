bad44300e34b2354982df89ee6bba57b58645300
Janos Brauer <janos.brauer@student.hpi.uni-potsdam.de>
2015-01-09 16:29:03 +0100

    Removal of Bookings

--- app/models/booking.rb
+++
@@ -1,8 +0,0 @@
-class Booking < ActiveRecord::Base
-  belongs_to :event
-  belongs_to :room
-
-  scope :approved, -> { where event: Event.approved }
-  scope :start_at_day, lambda { |date| where 'start BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day }
-
-end
