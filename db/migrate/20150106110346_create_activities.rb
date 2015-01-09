1763813553a16ff7d8e6816b1ee98649ca8599fb
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-01-09 11:03:32 +0100

    Add controller and task_name fields

--- db/migrate/20150106110346_create_activities.rb
+++ db/migrate/20150106110346_create_activities.rb
@@ -3,6 +3,8 @@ class CreateActivities < ActiveRecord::Migration
     create_table :activities do |t|
       t.string :username
       t.string :action
+      t.string :controller
+      t.string :task_name
       t.text :changed_fields
 
       t.timestamps
