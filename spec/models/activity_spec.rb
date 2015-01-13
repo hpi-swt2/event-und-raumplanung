b44598d5e578f30b703731274efbb933533d2dd8
mr-flannery <dennis_hempfing@gmx.de>
2015-01-13 19:55:17 +0100

    Added one test for the activity model. Description could be better, though. Still three failing tests in the events_show_spec.

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Activity, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # TODO: test für get_css_class schreiben
+  describe "when having an activity with certain values" do
+  	it "return the correct css class" do
+  		activity = Activity.new :controller => "tasks", :task_info => ["task", "done"]
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-ok")
+  		activity = Activity.new :controller => "tasks", :task_info => ["task", nil]
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-remove")
+  		activity = Activity.new :controller => "events", :action => "create"
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-plus")
+  		activity = Activity.new :controller => "events", :action => "update"
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-cog")
+  		activity = Activity.new :controller => "events", :action => "approve"
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-ok")
+  		activity = Activity.new :controller => "events", :action => "decline"
+  		expect(activity.get_css_class).to eq("glyphicon glyphicon-remove")
+  	end
+	end
 end
