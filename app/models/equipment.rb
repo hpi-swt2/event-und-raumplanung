b8aad3c131d2327d1046524f0655baee6d59014c
Sebastian.Rehfeldt <sebastian.rehfeldt@gmx.net>
2015-01-14 19:24:52 +0100

    added tests for equipment model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -20,13 +20,8 @@ class Equipment < ActiveRecord::Base
   scope :rooms, lambda { |name|
     terms = name.downcase.split(/\s+/)
     terms = terms.map { |e| ('%'+e.gsub('*', '%') + '%').gsub(/%+/, '%')}
-    
     room_ids = Room.where( terms.map { |term| "LOWER(rooms.name) LIKE ?"}.join(' AND '), *terms.map { |e| [e]} ).select("id")
-
-    
-    #room_id = Room.find_by_name("A-%")
-    
-    where('room_id IN (?)', room_ids) 
+    where('room_id IN (?)', room_ids)
   }
 
   scope :category, lambda { |category|
--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
-  factory :equipment do |f|
-    f.name 'beamer'
-    room
+  factory :correct_equipment, :class => Equipment do |f|
+    f.name 'Beamer HD 1'
+    f.category 'Beamer'
   end
+
+  factory :incorrect_equipment1, :class => Equipment do |f|
+    f.name 'Beamer HD 1'
+  end
+
+  factory :incorrect_equipment2, :class => Equipment do |f|
+    f.category 'Beamer'
+  end
+
 end
