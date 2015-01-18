a821d17e6795c4f9a37d9d727c58946535f65703
numberpi <daniel.stolpe@gmx.de>
2015-01-18 14:18:31 +0100

    Added tests for EventOccurrence, ref #198

--- spec/controllers/event_occurrence_controller_spec.rb
+++ spec/controllers/event_occurrence_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe EventOccurrenceController, :type => :controller do
+  include Devise::TestHelpers
 
+  let(:user) { create :user }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  describe "GET show" do
+
+    it "throws Not Found if event has no schedule" do
+      event = FactoryGirl.create(:event)
+      expect {
+        get :show, {:eventid => event.to_param}.merge(FactoryGirl.attributes_for(:event_occurrence))
+      }.to raise_error(ActionController::RoutingError)
+    end
+
+    it "throws Not Found if event has schedule, but occurrence doesn't match the schedule" do
+      weekly_recurring_event = FactoryGirl.create(:weekly_recurring_event)
+      expect {
+        get :show, {:eventid => weekly_recurring_event.to_param}.merge(FactoryGirl.attributes_for(:event_occurrence))
+      }.to raise_error(ActionController::RoutingError)
+    end
+
+    it "throws Not Found if event has no schedule" do
+      weekly_recurring_event = FactoryGirl.create(:weekly_recurring_event, :user_id => user.id)
+      get :show, {:eventid => weekly_recurring_event.to_param}.merge(FactoryGirl.attributes_for(:event_occurrence_for_weekly_event1))
+      expect(response).to be_success
+      expect(assigns(:event)).to be_a(EventOccurrence)
+    end
+  end
 end
