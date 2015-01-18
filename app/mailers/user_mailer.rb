8a3314eab91c07c57f079bad544501664b937623
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2015-01-18 18:33:24 +0100

    Added the accepted or declined event notification mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,4 +14,42 @@ class UserMailer < ActionMailer::Base
   	@task = task
   	mail(to: @user.email, subject: 'Your assignment to task ' + @task.name + ' was removed')
   end
+
+  def event_accepted_email_with_message(user, event, message)
+    @user = user
+    @message = message
+    @event = event
+
+    subject = 'Your event has been approved'
+
+    mail(to: @user.email, subject: subject)
+  end
+
+  def event_declined_email_with_message(user, event, message)
+    @user = user
+    @event = event
+    @message = message
+
+    subject = 'Your event has been declined'
+
+    mail(to: @user.email, subject: subject)
+  end
+
+  def event_accepted_email_without_message(user, event)
+    @user = user
+    @event = event
+
+    subject = 'Your event has been approved'
+
+    mail(to: @user.email, subject: subject)
+  end
+
+  def event_declined_email_without_message(user, event)
+    @user = user
+    @event = event
+
+    subject = 'Your event has been declined'
+
+    mail(to: @user.email, subject: subject)
+  end
 end
