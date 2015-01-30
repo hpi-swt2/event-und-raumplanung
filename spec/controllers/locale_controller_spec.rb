7e339a2f61bac43a636b4970bf45b842431a136e
mgrabs <martina.grabs@student.hpi.uni-potsdam.de>
2015-01-30 16:11:29 +0100

    added locale tests (locale_controller_spec.rb)

--- spec/controllers/locale_controller_spec.rb
+++ spec/controllers/locale_controller_spec.rb
@@ -3,10 +3,26 @@ require 'rails_helper'
 RSpec.describe LocaleController, :type => :controller do
 
   describe "GET change_locale" do
-    it "returns http success" do
-      get :change_locale
-      expect(response).to have_http_status(:success)
+	before(:each) { sign_in user }
+	let(:user) { create :user, language: "de" }
+
+    it "changes current user's locale to en" do
+    	get :change_locale, locale: "en"
+    	user.reload
+	    expect(user.language).to eq 'en'
     end
+
+    it "changes current user's locale to de if invalid locale entered" do
+    	get :change_locale, locale: "ru"
+    	user.reload
+	    expect(user.language).to eq 'de'
+    end
+
+    it "redirects to request referer" do
+	  	get :change_locale, locale: "en"
+      	expect(response).to redirect_to(root_path)
+    end
+
   end
 
 end
