8dcd63218cb4c00309f7c72656bdb4e3aefe74f9
Dustin Gedlich <dustin.gedlich@googlemail.com>
2015-01-30 15:07:42 +0100

    added reocurring events

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -2,47 +2,50 @@ class IcalController < ApplicationController
   before_action :authenticate_user!
 
   Mime::Type.register_alias "text/calendar", :ical
+
   # GET /ical/event/1
   def show_event
     @event = Event.find(params[:id])
-    @cal = Icalendar::Calendar.new
-    @cal.event do |e|
-      e.dtstart     = Icalendar::Values::Date.new(@event.starts_at)
-      e.dtend       = Icalendar::Values::Date.new(@event.ends_at)
-      e.summary     = @event.name
-      e.description = @event.description
-      e.ip_class    = "PRIVATE"
-    end
 
+    @cal = Icalendar::Calendar.new
+    add_event_to_cal @cal, @event
     @cal.publish
-    respond_to do |format|
-      format.ical { render text: @cal.to_ical }
-      format.ics { render text: @cal.to_ical }
-      format.html { render plain: @cal.to_ical }
-    end
-    # render plain: @cal.to_ical
+
+    render_ical @cal
   end
 
-  # GET  /ical/
+  # GET /ical/
   def show_my_events
     @events = Event.user(current_user_id)
 
     @cal = Icalendar::Calendar.new
     @events.each do |event|
-      @cal.event do |e|
-        e.dtstart     = Icalendar::Values::Date.new(event.starts_at)
-        e.dtend       = Icalendar::Values::Date.new(event.ends_at)
+      add_event_to_cal @cal, event
+    end
+    @cal.publish
+
+    render_ical @cal
+  end
+
+  private
+    def add_event_to_cal cal, event
+      cal.event do |e|
+        e.dtstart     = Icalendar::Values::Time.new(event.starts_at)
+        e.dtend       = Icalendar::Values::Time.new(event.ends_at)
+        e.organizer   = "mailto:" + User.find(event.user_id).email
         e.summary     = event.name
         e.description = event.description
         e.ip_class    = "PRIVATE"
+        e.rrule        = event.occurence_rule.to_ical
       end
     end
-    @cal.publish
-    respond_to do |format|
-      format.ical { render text: @cal.to_ical }
-      format.ics { render text: @cal.to_ical }
-      format.html { render plain: @cal.to_ical }
+
+    def render_ical cal
+      respond_to do |format|
+        format.ical { render text: @cal.to_ical }
+        format.ics { render text: @cal.to_ical }
+        format.html { render plain: @cal.to_ical }
+      end
     end
-  end
 
 end
