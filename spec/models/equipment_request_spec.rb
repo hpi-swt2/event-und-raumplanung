518cf12d6962fd06b801ac792f81796b339b5d65
Eva-Maria Herbst <eva-maria.herbst@student.hpi.uni-potsdam.de>
2015-02-03 20:10:51 +0100

    test factory of equipment_request #107

--- spec/models/equipment_request_spec.rb
+++ spec/models/equipment_request_spec.rb
@@ -1,5 +1,9 @@
+require_relative '../../app/models/equipment_request'
 require 'rails_helper'
 
 RSpec.describe EquipmentRequest, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    factory = FactoryGirl.build(:equipment_request)
+    expect(factory).to be_valid
+  end
 end
